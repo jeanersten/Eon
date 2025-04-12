b7e8c7473f0d91ae8e7a1d57a5fbf12e7fc9830f
Zaman Zulkarnaen <gavier.exe@gmail.com>
2025-04-12 18:36:24 +0700

    imp: Components as placeholder

--- source/Entity.h
+++ source/Entity.h
@@ -1,18 +1,35 @@
 #pragma once
 
+#include "Components.h"
+
+#include <memory>
+#include <string>
+
 class Entity
 {
+  friend class EntityManager;
+
 private:
+  Entity(unsigned int id, const std::string& tag);
+
   bool m_active;
 
   unsigned int m_id;
+  std::string m_tag;
 
 public:
-  Entity();
+  std::shared_ptr<CTransform> transform;
+  std::shared_ptr<CSprite> sprite;
+  std::shared_ptr<CRectangleCollider> rectangle_collider;
+  std::shared_ptr<CCircleCollider> circle_collider;
+  std::shared_ptr<CScore> score;
+  std::shared_ptr<CLifeSpan> life_span;
+  std::shared_ptr<CInput> input;
 
   bool isActive();
 
   unsigned int getID();
+  const std::string& getTag();
 
   void destroy();
 };
